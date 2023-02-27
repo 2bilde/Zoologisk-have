8a26f2b3422439d1fb39d985f0c5efdf6bac3884
Viktor Mehl <vikt2553@mail.hansenberg.dk>
2023-02-27 12:08:26 +0100

    Syg movement

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
+    private float speed = 1.0f;
+    public GameObject character;
+
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed;
+        }
+
     }
 }
